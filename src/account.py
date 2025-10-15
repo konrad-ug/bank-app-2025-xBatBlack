bfa93c8ec5b8a1e427f3d0d9d0c48cd4dc9054e5
Michał Downarowicz <mdownarowicz@INF.UG.EDU.PL>
2025-10-15 15:38:33 +0200

    feature2-5

--- src/account.py
+++ src/account.py
@@ -1,5 +1,20 @@
 class Account:
-    def __init__(self, first_name, last_name, balance):
+    def __init__(self, first_name, last_name, pesel, promo_code = None):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = balance
+        self.balance = 0
+        #self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+        if self.is_pesel_valid(pesel):
+            self.pesel = pesel
+        else:
+            self.pesel = "Invalid"
+        self.promo_code = promo_code
+    
+    def is_pesel_valid(self, pesel):
+        if len(pesel) == 11:
+            return True
+        return False
+
+    def check_promo(self):
+        if self.promo_code[0:5] == "PROM_" and int(self.pesel[0:2]) > 60:
+            self.balance += 50
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,21 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", 0)
+        account = Account("John", "Doe", "00000000000", "PROM_XYZ")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance >= 0
+        assert account.pesel == "00000000000"
+        assert account.promo_code == None or account.promo_code[0:5] == "PROM_"
+
+    def test_pesel_too_long(self):
+        account = Account("Jane", "Smith", "1432553253254355435")
+        assert account.pesel == "Invalid"
+    
+    def test_pesel_too_short(self):
+        account = Account("Jane", "Smith", "14325")
+        assert account.pesel == "Invalid"
+    
+    def test_promo(self):
+        account = Account("Jane", "Smith", "11111111111", "PROM_XYZ")
+        assert account.promo_code == "PROM_XYZ"
