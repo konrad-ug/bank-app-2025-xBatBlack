2e8a46e35d16ec1b6190e62cc9d50710b417bdd2
Michał Downarowicz <michaldownarowicz03@gmail.com>
2026-01-31 17:30:41 +0100

    Lab9, dodanie walidacji NIPu i zmiana testow kont firmowych

--- src/company_account.py
+++ src/company_account.py
@@ -1,3 +1,6 @@
+import os
+import requests
+from datetime import date
 from src.account import Account
 
 class CompanyAccount(Account):
@@ -5,16 +8,40 @@ class CompanyAccount(Account):
         super().__init__()
         self.name = name
         self.express_fee = 5
-        #self.nip = nip if self.is_nip_valid(nip) else "Invalid
+        self.balance = balance
 
         if self.is_nip_valid(nip):
-            self.nip = nip
+            if self._verify_nip_with_mf(nip):
+                self.nip = nip
+            else:
+                raise ValueError("Company not registered!!")
         else:
             self.nip = "Invalid"
-        self.balance = balance
         
     def is_nip_valid(self, nip):
         return len(nip) == 10
+    
+    def _verify_nip_with_mf(self, nip):
+        base_url = os.environ.get("BANK_APP_MF_URL", "https://wl-test.mf.gov.pl/")
+        today = date.today().strftime("%Y-%m-%d")
+        endpoint = f"{base_url}api/search/nip/{nip}?date={today}"
+        
+        try:
+            print(f"Sending request to: {endpoint}") 
+            response = requests.get(endpoint)
+            
+            print(f"MF API Response: {response.text}")
+            
+            if response.status_code == 200:
+                data = response.json()
+                if "result" in data and "subject" in data["result"]:
+                    return data["result"]["subject"]["statusVat"] == "Czynny"
+            
+            return False
+            
+        except requests.RequestException as e:
+            print(f"API Error: {e}")
+            return False
     
     def take_loan(self, amount):
         if self.balance >= amount * 2 and self._check_zus_transfer():
